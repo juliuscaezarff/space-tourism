b2d4730b0e5a5e6f1eebadf2e55baef6f5d38eb3
Julius Caezar <julius.caezar25@gmail.com>
2022-09-12 23:32:27 -0400

    add styles and button

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
 
+import './LinkButton.css'
+
 const LinkButton = ({to, text}) => {
 
   return(
-    <Link to={to}>{text}</Link>
+    <Link className='btn' to={to}>{text}</Link>
   )
 }
 
