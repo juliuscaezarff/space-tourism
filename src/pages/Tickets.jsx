0f7165c1bb8a067e8d7631135e4cdb4670d6738d
Julius Caezar <julius.caezar25@gmail.com>
2022-09-19 10:51:09 -0400

    page content

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,6 +1,28 @@
-const Tickets = () => {
+import './Tickets.css'
 
-  return <h1>Tickets</h1>
+const Tickets = () => {
+  return (
+    <section className="ticketContainer">
+      <h1>Inscreva-se para receber seu ticket!!</h1>
+
+      <p>E prepare-se para a mairo experiência da sua vida!</p>
+
+      <div>
+        <div>
+          <input type="text" placeholder="Digite seu e-mail" />
+        </div>
+        <div>
+          <input type="text" placeholder="Digite seu nome" />
+        </div>
+        <div>
+          <input type="password" placeholder="Digite sua senha" />
+        </div>
+        <div>
+          <input type="submit" value="Enviar" />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Tickets
